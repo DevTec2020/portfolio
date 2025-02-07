737dc3db16d13d6a36c6e08d9a6d5304aed5c672
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-02-06 21:19:33 -0300

    feat: criando Header

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -1,11 +1,11 @@
 export function Header() {
     return (
-        <nav>
+        <nav className="flex justify-evenly mt-10">
             <div>
-                <code>&lt;LeoBatista /&gt;</code>
+                <code>&lt;LeoBatista/&gt;</code>
             </div>
             <div>
-                <ul>
+                <ul className="flex gap-5">
                     <li>Sobre</li>
                     <li>Projetos</li>
                     <li>Contato</li>
