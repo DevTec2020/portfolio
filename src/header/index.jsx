54b805678810b8a2574b7582016dd5a5428b486d
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-02-06 21:13:50 -0300

    init: Subindo projeto

---
+++ src/header/index.jsx
@@ -0,0 +1,16 @@
+export function Header() {
+    return (
+        <nav>
+            <div>
+                <code>&lt;LeoBatista /&gt;</code>
+            </div>
+            <div>
+                <ul>
+                    <li>Sobre</li>
+                    <li>Projetos</li>
+                    <li>Contato</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
