c623bf1be5a9e347867f36ae18c7cfe7eb49c439
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-02-09 13:23:24 -0300

    feat: criando transition de cores no hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
 import { Header } from "./header";
 import img from "./assets/imgSection1.jpg";
+
+
 
 function App() {
   return (
@@ -10,16 +11,20 @@ function App() {
         style={{ backgroundImage: `url(${img})` }}
       >
         <Header />
-        <div className="absolute inset-y-0 left-56 flex flex-col justify-center space-y-5">
+        <div className="absolute inset-y-0 left-56 flex flex-col justify-center space-y-5 text-white">
           <h2 className="text-4xl font-light">Olá 👋! sou Leonardo Batista</h2>
           <h1 className="text-7xl font-semibold">Desenvolvedor web</h1>
-          <div className="space-x-7 pt-10">
-            <button className="px-6 py-2 bg-orange-500 text-white rounded">
+          <div className="space-x-10 pt-10">
+            <button className="px-10 py-4 transition delay-75 duration-500 bg-orange-400 hover:bg-white hover:text-black rounded font-semibold">
               Contato
             </button>
-            <a href="#" className="text-white">
-              Currículo
-            </a>
+
+            <button className="px-10 py-4 transition delay-75 duration-500 hover:bg-white hover:text-black rounded font-semibold">
+              <a href="#">
+                Currículo
+              </a>
+            </button>
+
           </div>
         </div>
       </section>
