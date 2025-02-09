d7b1c9d18805b75c4dd754f09c16b8f0e7a8a2a2
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-02-09 13:33:39 -0300

    feat: criando hover na logo

--- src/header/index.jsx
+++ src/header/index.jsx
@@ -4,21 +4,21 @@ export function Header() {
     return (
         <nav className="flex justify-around pt-10 font-semibold text-lg">
             <div>
-                <code className='text-orange-300 text-3xl'>
+                <code className='text-orange-300 text-3xl transition delay-75 duration-500 hover:text-white'>
                     &lt;LeoBatista/&gt;
                 </code>
             </div>
             <div>
-                <ul className="flex space-x-10 ">
-                    <li>
+                <ul className="flex space-x-10">
+                    <li className='transition delay-75 duration-500 hover:text-orange-300'>
                         <a href="Sobre">Sobre</a>    
                     </li>
                     
-                    <li>
+                    <li className='transition delay-75 duration-500 hover:text-orange-300'>
                         <a href="Projetos">Projetos</a>
                     </li>
                     
-                    <li>
+                    <li className='transition delay-75 duration-500 hover:text-orange-300'>
                         <a href="Contato">Contato</a>
                     </li>
                 </ul>
