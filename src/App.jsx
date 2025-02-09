342555973a4a3f33a4efe5b0e583243835a6d1b6
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-02-09 13:54:56 -0300

    feat: adicionando cargo dinamico

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
+import { useState, useEffect } from "react";
 import { Header } from "./header";
 import img from "./assets/imgSection1.jpg";
 
 
 
 function App() {
+  const [cargoDev, setCargoDev] = useState("")
+
+  useEffect(() => {
+    const cargo = ["Front end", "Back end", "Web", "Full Stack"]
+    let index = 0
+
+    const interval = setInterval(() => {
+      index = (index + 1) % cargo.length;
+      setCargoDev(cargo[index]);
+    }, 2000)
+
+    return () => clearInterval(interval);
+  },[]);
+
+
   return (
     <>
       <section
@@ -13,13 +29,19 @@ function App() {
         <Header />
         <div className="absolute inset-y-0 left-56 flex flex-col justify-center space-y-5 text-white">
           <h2 className="text-4xl font-light">Olá 👋! sou Leonardo Batista</h2>
-          <h1 className="text-7xl font-semibold">Desenvolvedor web</h1>
+          <h1 className="text-6xl font-semibold">
+            Desenvolvedor 
+            <span className="ms-4 text-orange-400">
+              {cargoDev}
+            </span>
+          </h1>
+
           <div className="space-x-10 pt-10">
-            <button className="px-10 py-4 transition delay-75 duration-500 bg-orange-400 hover:bg-white hover:text-black rounded font-semibold">
+            <button className="px-10 py-4 transition delay-75 duration-500 bg-orange-400 hover:bg-white hover:text-orange-400 rounded font-semibold">
               Contato
             </button>
 
-            <button className="px-10 py-4 transition delay-75 duration-500 hover:bg-white hover:text-black rounded font-semibold">
+            <button className="px-10 py-4 transition delay-75 duration-500 hover:bg-white hover:text-orange-400 rounded font-semibold">
               <a href="#">
                 Currículo
               </a>
