8a946ea0dca83dea46408feb721c6da88b5ab717
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-02-12 14:16:39 -0300

    feat: Atualizando layout de contato

--- src/contato/index.jsx
+++ src/contato/index.jsx
@@ -22,34 +22,40 @@ export function Contato(){
 
             {/* Texto */}
             <div>
-                <h1 className="text-4xl font-semibold text-orange-400">
+                <h1 className="text-4xl text-center font-semibold text-orange-400">
                     Contato
                 </h1>
 
-                <div className="mt-10 flex gap-4">
+                <div className="mt-10 space-y-4">
                   {[{
+                    description: "GitHub",
                     src: Git,
                     alt: "Git Ico",
                     href: "https://github.com/DevTec2020"
                   }, {
+                    description: "Email",
                     src: Email,
                     alt: "Email Ico",
                     href: "mailto:leonardo.silva.inf@gmail.com"
                   }, {
+                    description: "Linkedin",
                     src: Linkedin,
                     alt: "Linkedin Ico",
                     href: "https://www.linkedin.com/in/devtec-leobatista/"
                   }, {
+                    description: "Whatsapp",
                     src: Whatsapp,
                     alt: "Whatsapp Ico",
                     href: "https://wa.me/85985646542"
                   }].map((IconeContato, index) => (
-                    <div key={index}>
-                        <button type="button" className="border rounded-lg border-orange-500 shadow-sm shadow-orange-400">
-                        <a href={IconeContato.href} target="_blank">
-                            <img src={IconeContato.src} alt={IconeContato.alt} />
-                        </a>
+                    <div key={index} className="flex items-center gap-2">
+                        <button type="button" className="border rounded-lg border-[#C4A484] shadow-sm">
+                            <a href={IconeContato.href} target="_blank">
+                                <img src={IconeContato.src} alt={IconeContato.alt} />
+                            </a>
                         </button>
+                        <span className="text-[#C4A484] font-semibold">{IconeContato.description}</span>
+
                     </div>
 
                   ))}
