e7c7c361ab074914d621ef484b964369fab263ba
DevTec2020 <leonardo.silva.inf@gmail.com>
2025-02-13 11:25:01 -0300

    feat: Colocando os dados na descrição do botão

--- src/contato/index.jsx
+++ src/contato/index.jsx
@@ -8,7 +8,7 @@ import imgSection4 from "../assets/Profile.jpg";
 
 export function Contato(){
     return (
-      <section id="Contato" className="flex items-center justify-center min-h-screen bg-white px-8">
+      <section id="Contato" className="flex items-center justify-center min-h-screen bg-white px-10">
         <div className="grid grid-cols-2 max-w-6xl gap-8 items-center">
 
             {/* Imagem */}
@@ -21,37 +21,37 @@ export function Contato(){
             </div>
 
             {/* Texto */}
-            <div>
-                <h1 className="text-4xl text-center font-semibold text-orange-400">
+            <div className="">
+                <h1 className="text-4xl  font-bold text-[#C4A484]">
                     Contato
                 </h1>
 
                 <div className="mt-10 space-y-4">
                   {[{
-                    description: "GitHub",
+                    description: "/DevTec2020",
                     src: Git,
                     alt: "Git Ico",
                     href: "https://github.com/DevTec2020"
                   }, {
-                    description: "Email",
+                    description: "leonardo.silva.inf@gmail.com",
                     src: Email,
                     alt: "Email Ico",
                     href: "mailto:leonardo.silva.inf@gmail.com"
                   }, {
-                    description: "Linkedin",
+                    description: "/devtec-leobatista",
                     src: Linkedin,
                     alt: "Linkedin Ico",
                     href: "https://www.linkedin.com/in/devtec-leobatista/"
                   }, {
-                    description: "Whatsapp",
+                    description: "(85) 98564-6542",
                     src: Whatsapp,
                     alt: "Whatsapp Ico",
                     href: "https://wa.me/85985646542"
                   }].map((IconeContato, index) => (
                     <div key={index} className="flex items-center gap-2">
-                        <button type="button" className="border rounded-lg border-[#C4A484] shadow-sm">
+                        <button type="button" >
                             <a href={IconeContato.href} target="_blank">
-                                <img src={IconeContato.src} alt={IconeContato.alt} />
+                                <img className="border rounded-lg border-[#C4A484] shadow-sm" src={IconeContato.src} alt={IconeContato.alt} />
                             </a>
                         </button>
                         <span className="text-[#C4A484] font-semibold">{IconeContato.description}</span>
