ddf12aabf62d5aefd62c6bab680a377ee643a134
DevTec2020 <leonardo.silva.inf@gmail.com>
2026-01-16 14:31:35 -0300

    feat: Criando previa de projetos

---
+++ app/data/ProjetosPrev.ts
@@ -0,0 +1,26 @@
+import DevBurguer from "@/app/assets/Projetos/DevBurguer.webp"
+import PoupeMais from "@/app/assets/Projetos/PoupeMais.webp"
+
+const PROJETOSPREV = [
+                {
+                    image: PoupeMais,
+                    title: "Ótica Poupe Mais",
+                    tag: "Landing Page",
+                    stacks: ["React", "Tailwind", "Javascript", "SEO"],
+                    description: "Landing Page responsiva projetada para funcionar como portfólio  da ótica.",
+                    appUrl: "https://landing-page-otica-poupe-mais.vercel.app",
+                    codeUrl: "https://github.com/DevTec2020/LandingPage-Otica-Poupe-Mais"                    
+                },
+                 {
+                    image: DevBurguer,
+                    title: "DevBurguer",
+                    tag: "Sistema web",
+                    stacks: ["HTML", "Tailwind", "JavaScript"],
+                    description: "Cardápio Online com envio de pedidos diretamente para o WhatsApp.",
+                    appUrl: "https://dev-burguer-jade.vercel.app",
+                    codeUrl: "https://github.com/DevTec2020/DevBurguer"
+                },
+            ]
+
+
+export default PROJETOSPREV;
--- app/page.tsx
+++ app/page.tsx
@@ -17,19 +17,17 @@ import Otica from "@/app/assets/empresas/Logo_amarela.webp"
 import kalita from "@/app/assets/empresas/kalita.webp"
 
 
-import PROJETO from "./components/Projetos";
+import ProjetosPrevia from "./components/Projetos";
 
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub, faLinkedin, faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
-import { faHtml5, faCss3Alt, faJsSquare, faGitAlt, faPython, faReact, faNodeJs, faDocker, faFigma, faAws  } from '@fortawesome/free-brands-svg-icons';
 import { faDownload, faChevronDown, faEnvelope, faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 
 
 export default function Home(){
   const neonBlue = "#00d4ff"; // O Azul Neon
-  
 
   return (
     <>
@@ -186,26 +184,7 @@ export default function Home(){
       </section>
 
       {/* PROJETOS (Cards com Overlay Roxo no exemplo, agora Azul) */}
-      <section id="projetos" className="py-20 px-10 md:px-32">
-        <h3 className="text-3xl font-bold text-center mb-12 text-cyan-400 uppercase tracking-widest">Projetos</h3>
-        <div className="grid md:grid-cols-3 gap-8">
-          {PROJETO.map((projeto, item) => (
-            <div key={item} className="bg-[#151515] p-1 rounded-xl group overflow-hidden border border-white/5 hover:border-cyan-500/50 transition">
-              <div className="h-48 bg-neutral-800 rounded-t-lg mb-4 flex items-center justify-center italic text-gray-500">Preview Imagem</div>
-              <div className="p-4 text-center">
-                <p className="text-sm text-gray-400 mb-4 italic">{projeto.description}</p>
-                <button className="px-6 py-2 rounded-lg text-cyan-400 font-bold border border-cyan-500/50 
-             bg-cyan-500/10 backdrop-blur-md 
-             hover:bg-cyan-500/20 hover:text-cyan-300 hover:border-cyan-400
-             transition-all duration-300 shadow-[0_0_15px_rgba(0,212,255,0.1)] 
-             hover:shadow-[0_0_25px_rgba(0,212,255,0.3)]">
-                   Ver Projeto
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
+      <ProjetosPrevia/>
 
       {/* SOFT SKILLS (Box Centralizado) 
       <section className="py-20 px-10 flex justify-center">
